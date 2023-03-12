a18ba51c504cc9dc1a4ffd86de60ed1962137111
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-12 00:41:41 -0300

    working

--- src/TESTE/TableGenerator.jsx
+++ src/TESTE/TableGenerator.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import TableData from './TableData.json';
 
 const TableGenerator = () => {
+  
+  const rows = [];
+
+  for (let i = 0; i < 25; i++) {
+
+    const item = TableData[i];
+
+    // Skip the element at index 5
+    if (i === 5) {
+      continue;
+    }
+
+    const idStyle = i === 6 ? { color: 'red' } : {};
+
+    rows.push(
+
+      /* Check if item is falsy */
+      item && (
+        <td key={item.id}>
+          <div style={idStyle}>ID: {item.id}</div>
+          <div>Name: {item.name}</div>
+        </td>
+      )
+    );
+  }
 
   return (
-    
     <table>
-      <tbody>
-        {[...Array(4)].map((_, i) => (
-          <tr key={i}>
-            {[...Array(5)].map((_, j) => {
-              const index = i * 5 + j;
-              const item = TableData[index];
-              return (
-                <td key={j}>
-                  {item && (
-                    <>
-                      <div>ID: {item.id}</div>
-                      <div>Name: {item.name}</div>
-                    </>
-                  )}
-                </td>
-              );
-            })}
-          </tr>
-        ))}
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 };
