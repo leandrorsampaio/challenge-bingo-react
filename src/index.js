5c98ecc0322cf34edd86f0fd7937c9c8396148b9
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-13 14:34:13 -0300

    adding font family

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './styles/2_tools/fonts.css';
 import './styles/3_generic/reset.css';
+import './styles/4_elements/base.css';
 import './index.css';
+
 import App from './App';
 import TableGenerator from './TESTE/TableGenerator';
 
