941c174e99083a67ab43c1e67ded0d39b7f5fbea
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-14 14:38:56 -0300

    using SASS insteadt of pure CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/1_settings/colors.css';
-import './styles/2_tools/fonts.css';
-import './styles/3_generic/reset.css';
-import './styles/4_elements/base.css';
+import './styles/index.scss';
 
 import App from './App';
 import TableGenerator from './TESTE/TableGenerator';
