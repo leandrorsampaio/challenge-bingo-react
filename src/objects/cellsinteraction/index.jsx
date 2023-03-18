c5abe42b5cf12397b7236f3716f9a151fc05bad8
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-17 23:56:58 -0300

    making the checks working

--- src/objects/cellsinteraction/index.jsx
+++ src/objects/cellsinteraction/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Bingocard from '../bingocard';
+import Checkwinner from '../checkwinner';
 import './style.scss'
 
 const Cellsinteraction = (props) => {
@@ -9,27 +10,69 @@ const Cellsinteraction = (props) => {
     const [clickedIndexes, setClickedIndexes] = useState([]);
 
     function handleClick(index) {
-        setActiveIndex(index);
+
+        //setActiveIndex(index);
+        //console.log(index);
+
         setClickedIndexes((prevIndexes) => {
             if (prevIndexes.includes(index)) {
-              return prevIndexes.filter((i) => i !== index);
+                return prevIndexes.filter((i) => i !== index);
             } else {
-              return [...prevIndexes, index];
+                return [...prevIndexes, index];
             }
         });
+
     }
+
+
+
+
+
+
+    switch (true) {
+        case (clickedIndexes.includes(1) && clickedIndexes.includes(2) && clickedIndexes.includes(3) && clickedIndexes.includes(4) && clickedIndexes.includes(5)):
+            console.log('Linha 1');
+            break;
+        case (clickedIndexes.includes(6) && clickedIndexes.includes(7) && clickedIndexes.includes(8) && clickedIndexes.includes(9) && clickedIndexes.includes(10)):
+            console.log('Linha 2');
+            break;
+        case (clickedIndexes.includes(11) && clickedIndexes.includes(12) && clickedIndexes.includes(13) && clickedIndexes.includes(14) && clickedIndexes.includes(15)):
+            console.log('Linha 3');
+            break;
+        case (clickedIndexes.includes(16) && clickedIndexes.includes(17) && clickedIndexes.includes(18) && clickedIndexes.includes(19) && clickedIndexes.includes(20)):
+            console.log('Linha 4');
+            break;
+        case (clickedIndexes.includes(21) && clickedIndexes.includes(22) && clickedIndexes.includes(23) && clickedIndexes.includes(24) && clickedIndexes.includes(25)):
+            console.log('Linha 5');
+            break;
+
+        case (clickedIndexes.includes(1) && clickedIndexes.includes(7) && clickedIndexes.includes(13) && clickedIndexes.includes(19) && clickedIndexes.includes(25)):
+            console.log('DIAGONAL 1');
+            break;
+        case (clickedIndexes.includes(5) && clickedIndexes.includes(9) && clickedIndexes.includes(13) && clickedIndexes.includes(17) && clickedIndexes.includes(21)):
+            console.log('DIAGONAL 2');
+            break;
+        default:
+            console.log('NO');
+    }
+
 
 
 
 
     return (
         <div>
-            <Bingocard 
+            <Bingocard
                 activeIndex={activeIndex}
                 clickedIndexes={clickedIndexes}
                 onClick={handleClick}
-             />
-             <div>Clicked Indexes: {clickedIndexes.join(', ')}</div>
+            />
+
+            <Checkwinner />
+
+            <div>Clicked Indexes: {clickedIndexes.join(', ')}</div>
+
+
         </div>
     );
 };
