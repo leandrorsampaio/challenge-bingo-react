b20c7fde1ed98de2d47e17749c71f4cc2971b310
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-18 23:57:23 -0300

    more gifs

--- src/objects/gifswinner/index.jsx
+++ src/objects/gifswinner/index.jsx
@@ -6,6 +6,8 @@ const Gifswinner = (props) => {
     return (
         <>
            <img className="gifswinner--danceCar" src={require('../../assets/images/winner/cat-dance.gif')} alt="Cat dance gif" />
+           <img className="gifswinner--hireMe" src={require('../../assets/images/winner/hire-me.gif')} alt="Cat dance gif" />
+           <img className="gifswinner--postIt" src={require('../../assets/images/winner/post-it.gif')} alt="Cat dance gif" />
         </>
     );
 };
