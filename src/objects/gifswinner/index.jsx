d53adbde939a8da68524f64d76876ab6baaf9402
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-03-30 19:48:29 +0200

    (On going) adding refresh button in the confetti

--- src/objects/gifswinner/index.jsx
+++ src/objects/gifswinner/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
+import Refreshpage from './../refreshpage';
 import './style.scss'
 
 const Gifswinner = (props) => {
 
+    const [setValue] = useState('');
+    function handleValueChange(newValue) {
+        setValue(newValue);
+    }
+
     return (
         <>
-           <img className="gifswinner--danceCar" src={require('../../assets/images/winner/cat-dance.gif')} alt="Cat dance gif" />
-           <img className="gifswinner--hireMe" src={require('../../assets/images/winner/hire-me.gif')} alt="Cat dance gif" />
-           <img className="gifswinner--postIt" src={require('../../assets/images/winner/post-it.gif')} alt="Cat dance gif" />
+           <img className="gifswinner --danceCat" src={require('../../assets/images/winner/cat-dance.gif')} alt="Cat dance gif" />
+           <img className="gifswinner --hireMe" src={require('../../assets/images/winner/hire-me.gif')} alt="Cat dance gif" />
+           <img className="gifswinner --postIt" src={require('../../assets/images/winner/post-it.gif')} alt="Cat dance gif" />
+
+           <Refreshpage onValueChange={handleValueChange} />
+
         </>
     );
 };
