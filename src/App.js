1aa1cd41441aa336542f9cca8522bea9e3dcde2d
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-04-06 13:34:20 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import MyProvider from './objects/usercontextprovider';
 import MyConsumer from './objects/usercontentlayer';
 import Headerpage from './objects/headerpage';
